5b4cd845169016a314fea84adc8a9d92a2c4be67
isai cardoso <isai.cardoe@gmail.com>
2015-04-15 11:51:43 -0700

    done with video 2

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Simple To-DO List</title>
+        <link rel="stylesheet" type="text/css" href="css/main.css">
+    </head>
+    <body>
+        <div class="wrap">
+            
+        </div>  
+    </body>
+</html>
